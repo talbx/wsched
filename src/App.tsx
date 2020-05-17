871ee7d8111136d454a21161694376bf36159fcd
talbx <dev@talbx.de>
2020-05-17 22:09:42 +0200

    md5

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css'
-import {Button, Card, Checkbox, Container, FormCheckbox, Grid} from "semantic-ui-react";
+import {Button, Card, Checkbox, Container, Grid, Input} from "semantic-ui-react";
 import WeekTable2 from "./WeekTable2";
 import {generateDishes} from "./tools";
 
+var md5 = require('md5');
+
+
+const isValidPassphrase = (passphrase: string, fn: (input: string) => string, pred: (p: string, comp: string) => boolean): boolean => {
+    let transformed = fn(passphrase);
+    const hash = "asd";
+    return pred(transformed, hash);
+}
 
 
 function App() {
@@ -14,8 +22,10 @@ function App() {
     const [order, setOrder] = useState(false)
     const [veggie, setVeggie] = useState(false)
     const [dishes, setDishes] = useState([]);
+    const [passphrase, setPassphrase] = useState('');
 
     function generate() {
+
         //@ts-ignore
         setDishes(generateDishes(order, veggie, bbq));
     }
@@ -25,11 +35,15 @@ function App() {
             <Container>
                 <Card fluid color="teal" style={{padding: '1em 0em'}}>
                     <Card.Content>
-                        <Checkbox style={{padding: '0em 1em'}} toggle onChange={() => setBbq(!bbq)} label="Grillwetter? "/>
+                        <Checkbox style={{padding: '0em 1em'}} toggle onChange={() => setBbq(!bbq)}
+                                  label="Grillwetter? "/>
                         <Checkbox style={{padding: '0em 1em'}} toggle onChange={() => setOrder(!order)} checked={order}
-                                      label="Lust essen zu bestellen? "/>
-                        <Checkbox style={{padding: '0em 1em'}} toggle onChange={() => setVeggie(!veggie)} checked={veggie}
-                                      label="Vegetarische Woche? "/>
+                                  label="Lust essen zu bestellen? "/>
+                        <Checkbox style={{padding: '0em 1em'}} toggle onChange={() => setVeggie(!veggie)}
+                                  checked={veggie}
+                                  label="Vegetarische Woche? "/>
+                        <Input onChange={(event, data) => setPassphrase(data.value)} style={{padding: '0em 1em'}}
+                               size="mini" label='Passphrase' type='password'/>
                         <Button onClick={() => generate()} basic color="teal">Generieren</Button>
                     </Card.Content>
                     {
