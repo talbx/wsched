f4b19e72bad5c5a1e4e20f5474e6f36a284ef1a8
talbx <dev@talbx.de>
2020-06-15 22:04:25 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import {HomeComponent} from "./components/home/HomeComponent";
 import {RoadmapComponent} from "./components/roadmap/RoadmapComponent";
+import {Menu} from "semantic-ui-react";
 
 const App = () => {
+
+    const [active, setActive] = useState<string>('home');
+
     return (
         <BrowserRouter>
-            <div className="ui top menu">
-                <a className="item" href={"/"}>Home</a>
-                <a className="item" href={"/roadmap"}>Roadmap</a>
-            </div>
-            <div>
-                <a href="https://github.com/talbx/wsched">
-                    <div className="ribbon"><span>BETA</span></div>
-                </a>
-            </div>
+            <Menu pointing secondary color="teal">
+                <Menu.Item
+                    name='Home'
+                    active={active === 'home'}>
+                    <Link style={{color: 'black'}} to="/" onClick={() => setActive('home')}>Home</Link>
+                </Menu.Item>
+                <Menu.Item
+                    name='Roadmap'
+                    active={active === 'roadmap'}>
+                    <Link style={{color: 'black'}} to="/roadmap" onClick={() => setActive('roadmap')}>Roadmap</Link>
+                </Menu.Item>
+            </Menu>
+            <a href="https://github.com/talbx/wsched">
+                <div className="ribbon"><span>BETA</span></div>
+            </a>
             <div>
                 <Switch>
                     <Route path="/roadmap">
